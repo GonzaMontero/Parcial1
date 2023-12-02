a925273fbc9ec0ba497508a2065dfa311239cea4
Gonzalo Montero <gonmon654@gmail.com>
2023-12-02 13:25:56 -0300

    Commented pathing script

--- Assets/Scripts/Map/PathingAlternatives.cs
+++ Assets/Scripts/Map/PathingAlternatives.cs
@@ -8,10 +8,10 @@ public class PathingAlternatives {
 
     enum PathingTypes
     {
-        BreadthFirst,
-        DepthFirst,
-        Dijkstra,
-        AStar
+        BreadthFirst, //Alongside Depthfirst calculates the nodes in a specific direction and only checks for non occupied nodes
+        DepthFirst, //These are not usually optimal, but they are light on calculations, and don´t tend to choose the shortest path
+        Dijkstra, //This algorithm pick the unvisited slot with the lowest distance and chooses the route with the lowest cost
+        AStar //Very similar algorithm to dijkstra, but uses GetManhattanDistance to search exclusively in the direction of the target
     }
 
     private PathingTypes currentPathingTypes = PathingTypes.AStar;
@@ -25,20 +25,21 @@ public class PathingAlternatives {
         destinationPosition = destination.position;
 
         GridSlot currentSlot = origin;
-        while(currentSlot.position != destination.position)
+        while(currentSlot.position != destination.position) //Check if we did not reach the destination yet
         {
-            currentSlot = GetNextSlot(grid, currentSlot);
+            currentSlot = GetNextSlot(grid, currentSlot); // Get next slot from our currentSlot
 
-            if(currentSlot == null)
+            if(currentSlot == null) //if currentslot does not exist then the path by extension does not either
             {
                 return new List<Vector2Int>();
             }
 
-            for(int i = 0; i < currentSlot.adjacentPositionsID.Count; i++)
+            for(int i = 0; i < currentSlot.adjacentPositionsID.Count; i++) //for each position adjacent to our cell we check if it is available
             {
                 if (currentSlot.adjacentPositionsID[i] != GridUtils.invalidPosition)
                 {
-                    if (grid[currentSlot.adjacentPositionsID[i]].currentState == GridSlot.SlotState.Ready)
+                    if (grid[currentSlot.adjacentPositionsID[i]].currentState == GridSlot.SlotState.Ready) //if available we open it and add it to
+                                                                                                           //the list of possible nodes
                     {
                         grid[currentSlot.adjacentPositionsID[i]].Open(currentSlot.ID, currentSlot.totalWeight);
                         openSlotsID.Add(grid[currentSlot.adjacentPositionsID[i]].ID);
@@ -46,13 +47,14 @@ public class PathingAlternatives {
                 }
             }
 
-            currentSlot.currentState = GridSlot.SlotState.Close;
+            currentSlot.currentState = GridSlot.SlotState.Close; //close current slot and advance to the next one
             openSlotsID.Remove(currentSlot.ID);
             closedSlotsID.Add(currentSlot.ID);
         }
 
-        List<Vector2Int> path = GeneratePath(grid, currentSlot);
+        List<Vector2Int> path = GeneratePath(grid, currentSlot); //generate a path with the closed slots and current position
 
+        //reset all slots and return the finished path
         foreach(GridSlot slot in grid)
         {
             slot.Reset();
@@ -67,7 +69,7 @@ public class PathingAlternatives {
     {
         List<Vector2Int> path = new List<Vector2Int>();
 
-        while (currentSlot.idOfOpener != -1)
+        while (currentSlot.idOfOpener != -1) //check for currentslots which have the correct traversal id
         {
             path.Add(currentSlot.position);
             currentSlot = grid[currentSlot.idOfOpener];
@@ -81,7 +83,7 @@ public class PathingAlternatives {
 
     private GridSlot GetNextSlot(GridSlot[] grid, GridSlot currentSlot)
     {
-        switch(currentPathingTypes)
+        switch(currentPathingTypes) //depending on which algorithm we use, we get a different position
         {
             case PathingTypes.BreadthFirst:
                 return grid[openSlotsID[0]];
@@ -98,6 +100,7 @@ public class PathingAlternatives {
                         {
                             n = grid[openSlotsID[i]];
                             currentMaxWeight = grid[openSlotsID[i]].totalWeight;
+                            //Get lowest weight position slot
                         }
                     }
 
@@ -115,6 +118,7 @@ public class PathingAlternatives {
                         {
                             n = grid[openSlotsID[i]];
                             currentMaxWeight = grid[openSlotsID[i]].totalWeight;
+                            //Get lowest weight position slot which also points torwards the destination
                         }
                     }
 
