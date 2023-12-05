c46340bb2e94afd29163d0cc8dc6ad2d97448775
Gonzalo Montero <gonmon654@gmail.com>
2023-12-05 16:01:12 -0300

    Added new scripts and prefabs
    WIP - Check Movement

---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Managers
+{
+    public class MapManager : MonoBehaviour
+    {
+
+        [Header("Other Handlers")]
+        public VoronoiHandler VoronoiHandler;
+
+        [Header("Points of Interest & Map Settings")]
+        public Vector3Int MinerSpawnPosition;
+        public int MinesCount;
+        public Vector2Int MapSize;
+
+        [Header("Parent Transforms")]
+        public Transform MinesParents;
+        public Transform WallParent;
+
+        [Header("Easy Access Data")]
+        private List<Vector2Int> buildingPos = new();
+        public List<Vector2Int> BuildingPos => buildingPos;
+
+        private List<Vector2Int> minesPos = new();
+        public List<Vector2Int> MinesPos => minesPos;
+
+        private List<Vector2Int> minePositions;
+        public List<Vector2Int> MinePositions => minePositions;
+
+        private List<GameObject> minesList = new();
+        public List<GameObject> MinesList => minesList;
+
+        private GridSlot[] map;
+        public GridSlot[] Map => map;
+
+        private Vector2Int depositPos;
+        public Vector2Int DepositPos => depositPos;
+
+        public void Init(Vector2Int mapSize, GameObject mineGO)
+        {
+            InitBuildings();
+            InitMap(mapSize);
+
+            VoronoiHandler.Config();
+
+            for (int i = 0; i < MinesCount; i++)
+            {
+                SpawnMine(mineGO);
+            }
+
+            UpdateSectors();
+        }
+
+        private void InitBuildings()
+        {
+            buildingPos.Add(depositPos);
+
+            for (int i = 0; i < minesList.Count; i++)
+            {
+                Vector2Int pos = new Vector2Int((int)minesList[i].transform.position.x, (int)minesList[i].transform.position.y);
+                buildingPos.Add(pos);
+            }
+
+            for (int i = 0; i < WallParent.childCount; i++)
+            {
+                Transform wall = WallParent.GetChild(i);
+                Vector2Int pos = new Vector2Int((int)wall.transform.position.x, (int)wall.transform.position.y);
+                buildingPos.Add(pos);
+            }
+        }
+
+        private void InitMap(Vector2Int mapSize)
+        {
+            map = new GridSlot[mapSize.x * mapSize.y];
+            GridUtils.GridSize = new Vector2Int(mapSize.x, mapSize.y);
+            int id = 0;
+
+            for (int i = 0; i < mapSize.x; i++)
+            {
+                for (int j = 0; j < mapSize.y; j++)
+                {
+                    map[id] = new GridSlot(id, new Vector2Int(j, i));
+                    map[id].SetWeight(Random.Range(1, 6));
+
+                    for (int k = 0; k < buildingPos.Count; k++)
+                    {
+                        if (map[id].position == buildingPos[k])
+                        {
+                            map[id].currentState = SlotStates.Obstacle;
+                        }
+                    }
+
+                    id++;
+                }
+            }
+        }
+
+        private void SpawnMine(GameObject mineGO)
+        {
+            int x = Random.Range(1, GridUtils.GridSize.x - 1);
+            int y = Random.Range(1, GridUtils.GridSize.y - 1);
+            Vector2Int pos = new Vector2Int(x, y);
+
+            for (int i = 0; i < buildingPos.Count; i++)
+            {
+                if (pos == buildingPos[i])
+                {
+                    SpawnMine(mineGO);
+                    return;
+                }
+            }
+
+            Vector3 posVec3 = new Vector3(pos.x, pos.y, 0);
+            var go = Instantiate(mineGO, posVec3, Quaternion.identity, MinesParents);
+
+            minesList.Add(go);
+            buildingPos.Add(pos);
+            minesPos.Add(pos);
+        }
+
+        private GridSlot[] GetMap()
+        {
+            return map;
+        }
+
+        public void UpdateSectors()
+        {
+            List<(Vector2, float)> minesPos = new List<(Vector2, float)>();
+            foreach (var mine in minesList)
+            {
+                float weight = Random.Range(1, 6);
+                minesPos.Add((mine.transform.position, weight));
+            }
+            VoronoiHandler.UpdateSectors(minesPos);
+        } 
+    }
+}
+
---
+++ Assets/Scripts/Managers/MinerManager.cs
@@ -0,0 +1,55 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Managers
+{
+    public class MinerManager : MonoBehaviour
+    {
+        public ConcurrentBag<Miner> Miners = new();
+        public ParallelOptions ParallelOptions;
+        public Vector2Int DepositPosition;
+        public Vector2Int RestPosition;
+        public float DeltaTime;
+        public Action OnUpdateWeight;
+
+        public void Init(Vector2Int depositPosition, Vector2Int restPosition)
+        {
+            ParallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 6 };
+
+            DepositPosition = depositPosition;
+            RestPosition = restPosition;
+        }
+
+        public void UpdateMiners()
+        {
+            DeltaTime -= Time.deltaTime;
+
+            foreach (var miner in Miners)
+            {
+                if (miner.updatePos)
+                {
+                    miner.transform.position = miner.CurrentPos;
+                    miner.updatePos = false;
+                }
+            }
+
+            Parallel.ForEach(Miners, ParallelOptions, miner => { miner.UpdateMiner(); });
+        }
+
+        public void UpdateWeight(Vector2Int nodePos, int nodeWeight)
+        {
+            Parallel.ForEach(Miners, ParallelOptions, miner => { miner.UpdateWeight(nodePos, nodeWeight); });
+        }
+
+        public void AbruptExit()
+        {
+            Parallel.ForEach(Miners, ParallelOptions, miner => { miner.ExitMiner(); });
+        }       
+    }
+}
+
